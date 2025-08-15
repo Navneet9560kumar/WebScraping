7b04eaaa5e430d3312e436263ee8eb5aa3a5d5d3
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-08-15 13:56:03 +0530

    pdf extrate

---
+++ Websc/day_10.py
@@ -0,0 +1,22 @@
+import fitz  # PyMuPDF
+
+def read_pdf(file_path):
+    doc = fitz.open(file_path)
+    all_text = ""
+
+    for page_num in range(len(doc)):
+        page = doc[page_num]
+        all_text += page.get_text()
+
+    doc.close()
+    return all_text
+
+if __name__ == "__main__":
+    file_path = r"C:\Users\Lenovo\Desktop\webScraping\websc\nodejs.pdf"  # Change to your PDF path
+    try:
+        content = read_pdf(file_path)
+        print("-" * 30)
+        print(content)
+        print("-" * 30)
+    except Exception as e:
+        print("Failed to read PDF:", e)
